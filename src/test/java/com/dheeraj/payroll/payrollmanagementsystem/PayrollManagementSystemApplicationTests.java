ef488766c6de808606c306740d98bc6c378bc4a1
Dheeraj Basude <dheerajbe123@gmail.com>
2025-12-04 00:21:26 +0530

    first commit

---
+++ src/test/java/com/dheeraj/payroll/payrollmanagementsystem/PayrollManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dheeraj.payroll.payrollmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
