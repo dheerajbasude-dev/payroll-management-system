1dc2c268ea42a2c5d2347eeeb78341ae81efc705
Dheeraj Basude <dheerajbe123@gmail.com>
2025-12-04 20:43:05 +0530

    feat: Employee and Payroll document added POJO class

--- src/main/java/document/Employee.java
+++ src/main/java/document/Employee.java
@@ -1,4 +1,33 @@
 package document;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+
+@Schema(
+        name = "Employee",
+        description = "Schema to hold Employee information"
+)
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@Document(collection = "employees")
 public class Employee {
+    @Id
+    private String id;
+
+    private String name;
+    private Integer age;
+    private String gender;
+    private String designation;
+    private Integer rating;
+    private Double salary;
+
 }
--- src/main/java/document/Payroll.java
+++ src/main/java/document/Payroll.java
@@ -1,4 +1,45 @@
 package document;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Schema(
+        name = "Payroll",
+        description = "Schema to hold Payroll information"
+)
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@Document(collection = "payrolls")
 public class Payroll {
+    @Id
+    private String id;
+
+    @JsonIgnore
+    @Indexed
+    private String employeeId;
+
+    private String payDate;
+    private Double grossSalary;
+    private Double taxAmount;
+    private Double netSalary;
+
+    @JsonIgnore
+    private String payMonth;
+
+    @JsonIgnore
+    private Integer payYear;
+
+    @Transient
+    private Employee employee;
+
 }
