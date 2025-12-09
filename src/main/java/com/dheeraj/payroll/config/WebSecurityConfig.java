da3d57ef27a6d19774c48757f784433e98610b92
Dheeraj475 <113779503+Dheeraj475@users.noreply.github.com>
2025-12-09 22:20:39 +0530

    dep: updated swagger open api version to 2.7.0, and added EV variables

--- src/main/java/com/dheeraj/payroll/config/WebSecurityConfig.java
+++ src/main/java/com/dheeraj/payroll/config/WebSecurityConfig.java
@@ -73,7 +73,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8111"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
