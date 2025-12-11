b49cb7afca299e1bfb57831b7cd6f08096d0dc28
Dheeraj475 <113779503+Dheeraj475@users.noreply.github.com>
2025-12-11 13:00:32 +0530

    chore: web security url render updated

--- src/main/java/com/dheeraj/payroll/config/WebSecurityConfig.java
+++ src/main/java/com/dheeraj/payroll/config/WebSecurityConfig.java
@@ -73,7 +73,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("https://payroll-management-system-ntwn.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
